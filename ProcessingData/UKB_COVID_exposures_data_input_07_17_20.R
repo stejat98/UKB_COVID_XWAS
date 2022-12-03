5394759039d4c544b20967cf543b2038eb9fd877
stejat98 <stejat@icloud.com>
2022-12-03 16:15:37 -0500

    Update and rename UKB_Whites_gCOVID_data_input_07_17_20.R to UKB_COVID_exposures_data_input_07_17_20.R

--- ProcessingData/UKB_COVID_exposures_data_input_07_17_20.R
+++ ProcessingData/UKB_COVID_exposures_data_input_07_17_20.R
@@ -1,3 +1,10 @@
+## Sivateja Tangirala
+
+# The following script puts together the input dataframe consisting of exposures, outcome of COVID-19 positivity
+# and baseline covariates adjusted for during the analysis.
+
+## assumes all input dfs used for script are in same directory ~/UKB_COVID19/
+
 
 library(tidyverse)
 library(lubridate)
@@ -8,13 +15,17 @@ library(broom)
 library(dummies)
 library(pROC)
 
+# setting the working directory to where the input data is stored and data saved to 
+# system("mkdir UKB_COVID19")
+
+setwd('~/UKB_COVID19/')
 
 
 ## load environmental factor data
 ukb_process_avg_df <- readRDS("ukb_environment_reformatted_08_01_19.RDS")
 
 ## load covid test positivity data
-covid_data <- read_delim("/n/groups/patel/uk_biobank/covid_death_071720/22881/covid19_result_071720.txt",delim = "\t")
+covid_data <- read_delim("covid19_result_071720.txt",delim = "\t")
 
 covid_data$specdate <- as.Date(covid_data$specdate,"%d/%m/%y")
 
@@ -54,7 +65,7 @@ ukb9512_plyr <- read_rds("ukb9512_avg_pheno_values.RDS")
 covid_ukb9512_full <- left_join(covid_data_exposures,ukb9512_plyr, by = c("eid" = "f.eid"))
 
 ## load processed biomarker data
-biomarkers_median_processed <- read_rds("/n/groups/patel/sivateja/UKB/COVID19/biomarkers_median_processed.RDS")
+biomarkers_median_processed <- read_rds("biomarkers_median_processed.RDS")
 
 covid_ukb9512_full <- left_join(covid_ukb9512_full, biomarkers_median_processed, by = "eid")
 
@@ -67,7 +78,7 @@ covid_ukb9512_full$f.31.0.0 <- factor(covid_ukb9512_full$f.31.0.0,ordered=F)
 old_gpcs_subset <- covid_ukb9512_full[,c(1,grep("f.22009.0.1",colnames(covid_ukb9512_full),fixed=T)[1]:grep("f.22009.0.15",colnames(covid_ukb9512_full),fixed=T))]
 covid_ukb9512_full <- covid_ukb9512_full[,-c(grep("f.22009.0.1",colnames(covid_ukb9512_full),fixed=T)[1]:grep("f.22009.0.15",colnames(covid_ukb9512_full),fixed=T))]
 
-ukb26323 <- read_delim("/n/groups/patel/uk_biobank/pheno_26323/ukb26323.tab",delim="\t")
+ukb26323 <- read_delim("ukb26323.tab",delim="\t")
 ## select f.eid and 40 GPCs
 ukb26323_filtered <- ukb26323[,c(1,5334:5373)]
 
@@ -81,7 +92,8 @@ covid_ukb9512_full_40GPCs <- covid_ukb9512_full_40GPCs[,-grep("avg_",colnames(co
 covid_ukb9512_full_40GPCs <- left_join(covid_ukb9512_full_40GPCs,ukb26323_date_attending_assessment_center,by=c("eid" = "f.eid"))
 
 ## load other infectious markers data
-ukb29961_avg <- readRDS("/n/groups/patel/sivateja/UKB/ukb29961_avg.RDS")
+
+ukb29961_avg <- readRDS("ukb29961_avg.RDS")
 
 ukb29961_avg <- ukb29961_avg[,c(1,grep("avg",colnames(ukb29961_avg)))]
 
@@ -90,14 +102,14 @@ infectious_markers_data <- ukb29961_avg[,1:grep("avg_f.23044",colnames(ukb29961_
 
 covid_ukb9512_full_40GPCs <- left_join(covid_ukb9512_full_40GPCs,infectious_markers_data,by=c("eid"="f.eid"))
 
-saveRDS(covid_ukb9512_full_40GPCs,"/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_ukb9512_full_40GPCs_07_17_20.RDS")
+saveRDS(covid_ukb9512_full_40GPCs,"covid_ukb9512_full_40GPCs_07_17_20.RDS")
 
 
 ## updated 07/16/2020
 
-covid_ukb9512_full_40GPCs <- readRDS("/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_ukb9512_full_40GPCs_07_17_20.RDS")
+covid_ukb9512_full_40GPCs <- readRDS("covid_ukb9512_full_40GPCs_07_17_20.RDS")
 
-ukb34521 <- read_csv("/n/groups/patel/uk_biobank/main_data_34521/ukb34521.csv")
+ukb34521 <- read_csv("ukb34521.csv")
 
 
 ## select health and disease indicator columns
@@ -109,10 +121,10 @@ ukb34521_health_covars_data_filtered <- ukb34521_health_covars_data %>% filter(!
   filter(! `2443-0.0` %in% c(-1,-3)) %>% filter(! `2453-0.0` %in% c(-1,-3)) %>%  filter(! `6150-0.0` == -3) %>% 
   filter(!`6152-0.0` == -3)
 
-saveRDS(ukb34521_health_covars_data_filtered, "/n/scratch3/users/s/st320/UKB_COVID_XWAS/ukb34521_health_covars_data_filtered.RDS")
+saveRDS(ukb34521_health_covars_data_filtered, "ukb34521_health_covars_data_filtered.RDS")
 
-ukb34521_health_covars_data_filtered <- readRDS("/n/scratch3/users/s/st320/UKB_COVID_XWAS/ukb34521_health_covars_data_filtered.RDS")
-saveRDS(ukb34521_health_covars_data_filtered,"/n/groups/patel/sivateja/UKB/COVID19/ukb34521_health_covars_data_filtered.RDS")
+ukb34521_health_covars_data_filtered <- readRDS("ukb34521_health_covars_data_filtered.RDS")
+saveRDS(ukb34521_health_covars_data_filtered,"ukb34521_health_covars_data_filtered.RDS")
 
 ukb34521_health_covars_data_filtered <- ukb34521_health_covars_data_filtered %>% rename(x_2178 = `2178-0.0`,x_2443 = `2443-0.0`,x_2453 = `2453-0.0`,x_6150 = `6150-0.0`, x_6152 = `6152-0.0`)
 
@@ -138,12 +150,12 @@ ukb34521_health_covars_data_filtered_proc$x_2453 <- as.factor(ukb34521_health_co
 
 ukb34521_health_covars_data_filtered_proc[,5:length(colnames(ukb34521_health_covars_data_filtered_proc))] <- lapply(ukb34521_health_covars_data_filtered_proc[,5:length(colnames(ukb34521_health_covars_data_filtered_proc))],as.logical)
 
-saveRDS(ukb34521_health_covars_data_filtered_proc, "/n/scratch3/users/s/st320/ukb34521_health_covars_data_filtered_proc.RDS")
+saveRDS(ukb34521_health_covars_data_filtered_proc, "ukb34521_health_covars_data_filtered_proc.RDS")
 
-ukb34521_health_covars_data_filtered_proc <- readRDS("/n/scratch3/users/s/st320/ukb34521_health_covars_data_filtered_proc.RDS")
-saveRDS(ukb34521_health_covars_data_filtered_proc, "/n/groups/patel/sivateja/UKB/COVID19/ukb34521_health_covars_data_filtered_proc.RDS")
+ukb34521_health_covars_data_filtered_proc <- readRDS("ukb34521_health_covars_data_filtered_proc.RDS")
+saveRDS(ukb34521_health_covars_data_filtered_proc, "ukb34521_health_covars_data_filtered_proc.RDS")
 
-ukb34521_health_covars_data_filtered_proc <- readRDS("/n/groups/patel/sivateja/UKB/COVID19/ukb34521_health_covars_data_filtered_proc.RDS")
+ukb34521_health_covars_data_filtered_proc <- readRDS("ukb34521_health_covars_data_filtered_proc.RDS")
 
 
 covid_ukb9512_full_40GPCs_updated <- left_join(covid_ukb9512_full_40GPCs,ukb34521_health_covars_data_filtered_proc,by = "eid")
@@ -152,25 +164,26 @@ covid_ukb9512_full_40GPCs_updated <- left_join(covid_ukb9512_full_40GPCs,ukb3452
 assessment_result_date_interv = interval(covid_ukb9512_full_40GPCs_updated$f.53.0.0, covid_ukb9512_full_40GPCs_updated$result_date)
 covid_ukb9512_full_40GPCs_updated$time_diff_years = assessment_result_date_interv %>% as.numeric('years')
 
-saveRDS(covid_ukb9512_full_40GPCs_updated ,"/n/groups/patel/sivateja/UKB/COVID19/covid_ukb9512_full_40GPCs_updated_07_17_20.RDS")
+saveRDS(covid_ukb9512_full_40GPCs_updated ,"covid_ukb9512_full_40GPCs_updated_07_17_20.RDS")
 
 
-##
+## vector of exposures
 load(file = "ukb_exposures_pewas.RData")
 ukb_exposures_covid <- c(ukb_exposures_pewas,colnames(ukb34521_health_covars_data_filtered_proc)[-1],colnames(biomarkers_median_processed)[171:233])
 
-save(ukb_exposures_covid,file="/n/groups/patel/sivateja/UKB/COVID19/ukb_exposures_covid.RData")
+save(ukb_exposures_covid,file="ukb_exposures_covid.RData")
 
 
+## baseline covariates
 load("all_baseline_col_names.RData")
 all_baseline_col_names_covid_xwas <- c(all_baseline_col_names,"f.21000.0.0",colnames(ukb34521_health_covars_data_filtered_proc)[-1])
 
 save(all_baseline_col_names_covid_xwas,file="all_baseline_col_names_COVID_XWAS.RData")
 
-
+## full list baseline covariates for COVID-19 analysis
 load("all_baseline_col_names_COVID_XWAS.RData")
 all_baseline_col_names_covid_xwas <- all_baseline_col_names_covid_xwas[-c(47:length(all_baseline_col_names_covid_xwas))]
 all_baseline_col_names_covid_xwas[c(2,3)] <- c("Age","Age_squared")
 
-save(all_baseline_col_names_covid_xwas,file="/n/groups/patel/sivateja/UKB/COVID19/all_baseline_col_names_covid_xwas.RData")
+save(all_baseline_col_names_covid_xwas,file="all_baseline_col_names_covid_xwas.RData")
 
