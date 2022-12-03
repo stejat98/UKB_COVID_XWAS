60b544c6e0fcab4049309e43bb8fc95eb5e82e0b
stejat98 <stejat@icloud.com>
2022-12-03 16:26:20 -0500

    Update covid_input_process_data_script_07_17_20_updated.R

--- ProcessingData/covid_input_process_data_script_07_17_20_updated.R
+++ ProcessingData/covid_input_process_data_script_07_17_20_updated.R
@@ -3,35 +3,22 @@ library(dummies)
 
 ## USING  VERSION OF DATA AS OF JULY 17 2020
 
-hesin_diag_table <- read_delim("/n/groups/patel/uk_biobank/covid_death_071720/22881/hesin_diag_071720.txt",delim = "\t")
+hesin_diag_table <- read_delim("hesin_diag_071720.txt",delim = "\t")
 
 ## load covid test positivity data
-covid_data <- read_delim("/n/groups/patel/uk_biobank/covid_death_071720/22881/covid19_result_071720.txt",delim = "\t")
+covid_data <- read_delim("covid19_result_071720.txt",delim = "\t")
 
 covid_data$specdate <- as.Date(covid_data$specdate,"%d/%m/%y")
 
 
-covid_ukb9512_full_40GPCs_updated <- readRDS("/n/groups/patel/sivateja/UKB/COVID19/covid_ukb9512_full_40GPCs_updated_07_17_20.RDS")
+covid_ukb9512_full_40GPCs_updated <- readRDS("covid_ukb9512_full_40GPCs_updated_07_17_20.RDS")
 
-ukb26323 <- read_delim("/n/groups/patel/uk_biobank/pheno_26323/ukb26323.tab",delim="\t")
+ukb26323 <- read_delim("ukb26323.tab",delim="\t")
 
 ## create dataframe with age column and generate age^2 column
 age_data <- ukb26323 %>% select(f.eid,f.21022.0.0) %>% mutate(Age = f.21022.0.0, Age_squared = f.21022.0.0^2)
 
 covid_ukb9512_full_40GPCs_updated_age_data <- left_join(covid_ukb9512_full_40GPCs_updated, age_data, by = c("eid" = "f.eid"))
-
-## filter for hospitalization due to COVID 19 (ICD-10 codes: U07.1 AND U07.2)
-# covid_hosp_data  <- hesin_diag_table %>% filter(diag_icd10 %in% c("U071","U072"))
-# covid_hosp_data_patient_ids <- unique(covid_hosp_data$eid)
-
-
-#all_data_patient_ids <- unique(hesin_diag_table$eid)
-## include all ids from covid positivity data
-# all_data_patient_ids <- unique(covid_ukb9512_full_40GPCs_updated_age_data$eid)
-# 
-# covid_hosp_status <- ifelse(all_data_patient_ids %in% covid_hosp_data_patient_ids,1,0)
-# 
-# covid_hosp_status_data_processed <- as.data.frame(cbind(eid = all_data_patient_ids, covid_hosp_status ))
 
 ## function to check if there is at least one hospitalization (in-patient status) for a given participant  
 covid_hosp_extract_status <- function(id,data){
@@ -53,18 +40,18 @@ covid_hosp_status_data_processed <- bind_cols(eid = covid_test_patient_ids, covi
 
 covid_ukb9512_full_40GPCs_updated_age_data <- left_join(covid_ukb9512_full_40GPCs_updated_age_data,covid_hosp_status_data_processed,by="eid")
 
-saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"/n/groups/patel/sivateja/UKB/COVID19/covid_hosp_status_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
+saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"covid_hosp_status_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
 
-covid_ukb9512_full_40GPCs_updated_age_data <- readRDS("/n/groups/patel/sivateja/UKB/COVID19/covid_hosp_status_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
+covid_ukb9512_full_40GPCs_updated_age_data <- readRDS("covid_hosp_status_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
 
-death_cause_data <- read_delim("/n/groups/patel/uk_biobank/covid_death_071720/22881/death_cause_071720.txt",delim = "\t")
+death_cause_data <- read_delim("death_cause_071720.txt",delim = "\t")
 
 ## filter for death due to COVID 19 (ICD-10 codes: U07.1 AND U07.2)
 covid_death_cause_data  <- death_cause_data %>% filter(cause_icd10 %in% c("U071","U072"))
 covid_death_cause_data_patient_ids <- unique(covid_death_cause_data$eid)
 
 
-#all_death_data_patient_ids <- unique(death_cause_data$eid)
+
 ## include all ids from covid positivity data
 all_death_data_patient_ids <- unique(covid_ukb9512_full_40GPCs_updated_age_data$eid)
 
@@ -74,11 +61,11 @@ covid_death_data_processed <- as.data.frame(cbind(eid = all_death_data_patient_i
 
 covid_ukb9512_full_40GPCs_updated_age_data <- left_join(covid_ukb9512_full_40GPCs_updated_age_data,covid_death_data_processed,by="eid")
 
-saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
+saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
 
 ## update dataframe with correct variable codings
 
-data <- readRDS("/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
+data <- readRDS("covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
 
 ethnicity_categ_to_remove <- names(table(data$f.21000.0.0)[which(table(data$f.21000.0.0) < 20)])
 data  <- data  %>% filter(! f.21000.0.0 %in% c("Do not know","Prefer not to answer","Other ethnic group",ethnicity_categ_to_remove))
@@ -97,4 +84,4 @@ data$x_738 <- relevel(data$x_738,ref = 5)
 
 data$f.31.0.0 <- factor(data$f.31.0.0,ordered=F)
 
-saveRDS(data,"/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
+saveRDS(data,"covid_death_ukb9512_full_40GPCs_updated_07_17_2020.RDS")
