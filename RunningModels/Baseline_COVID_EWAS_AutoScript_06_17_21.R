b3b951a383b86603a5c9aa147b9f222a9f8fb2fd
stejat98 <stejat@icloud.com>
2022-12-05 03:15:58 -0500

    Update Baseline_COVID_EWAS_AutoScript_06_17_21.R

--- RunningModels/Baseline_COVID_EWAS_AutoScript_06_17_21.R
+++ RunningModels/Baseline_COVID_EWAS_AutoScript_06_17_21.R
@@ -18,17 +18,17 @@ library(broom)
 library(dummies)
 
 
-source("/home/st320/Baseline_EWAS_Logistic_Functions_Script.R") 
+source("Baseline_EWAS_Logistic_Functions_Script.R") 
 
-data <- readRDS("/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_death_ukb9512_full_40GPCs_updated_06_17_21_processed.RDS")
+data <- readRDS("covid_death_ukb9512_full_40GPCs_updated_06_17_21_processed.RDS")
 
-load(file="/n/scratch3/users/s/st320/UKB_COVID_XWAS/all_baseline_col_names_covid_xwas.RData")
+load(file="all_baseline_col_names_covid_xwas.RData")
 ## add ethnicity vars to other baseline covariates
 all_baseline_col_names_covid_xwas <- c(all_baseline_col_names_covid_xwas[1:45],colnames(data)[605:619])
 adjustments <- all_baseline_col_names_covid_xwas
 
 
-load(file = "/n/scratch3/users/s/st320/UKB_COVID_XWAS/ukb_exposures_covid.RData")
+load(file = "ukb_exposures_covid.RData")
 
 for (exposureCol in ukb_exposures_covid){
   if(!is.numeric(data[[exposureCol]])){
@@ -44,4 +44,4 @@ reformatted_exposures[grep("median",reformatted_exposures)] <- sprintf("INT_%s",
 
 ## run EWAS (poisson) for specified phenotype (Baseline covariates) [extract p-values, etc.]
 EWASLogistic(data=data, depvar = as.character("result")
-     ,adjustments = adjustments,exposures=reformatted_exposures,offset_var = "time_diff_years",outFileName = "/home/st320/UKB_COVID_XWAS/covid_ewas_02_02_2021_poisson_log_glm_results_09_15_21")
+     ,adjustments = adjustments,exposures=reformatted_exposures,offset_var = "time_diff_years",outFileName = "covid_ewas_02_02_2021_poisson_log_glm_results_09_15_21")
