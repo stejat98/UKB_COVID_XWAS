d7d340267923f138302724bf73417e703c4a1fd1
stejat98 <stejat@icloud.com>
2022-12-05 03:03:26 -0500

    Update covid_input_process_hosp_death_data_script_06_17_21.R

--- ProcessingData/covid_input_process_hosp_death_data_script_06_17_21.R
+++ ProcessingData/covid_input_process_hosp_death_data_script_06_17_21.R
@@ -5,23 +5,29 @@
 
 ## assumes all input dfs used for script are in same directory ~/UKB_COVID19/
 
+# setting the working directory to where the input data is stored and data saved to 
+# system("mkdir UKB_COVID19")
+
+setwd('~/UKB_COVID19/')
+
+
 
 library(tidyverse)
 library(dummies)
 
-hesin_diag_table <- read_delim("/home/st320/UKB_COVID_Analysis_02_21/hesin_diag_020721.txt",delim = "\t")
+hesin_diag_table <- read_delim("hesin_diag_020721.txt",delim = "\t")
 
 
 ## load covid test positivity data
-covid_data <- read_delim("/home/st320/UKB_COVID_Analysis_02_21/covid19_result_2_2021.txt",delim = "\t")
+covid_data <- read_delim("covid19_result_2_2021.txt",delim = "\t")
 covid_data$specdate <- as.Date(covid_data$specdate,"%d/%m/%y")
 ## filter out cases before 07/17/2020
 covid_data <- covid_data %>% filter(specdate > as.Date("2020-07-17"))
 
 
-covid_ukb9512_full_40GPCs_updated <- readRDS("/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+covid_ukb9512_full_40GPCs_updated <- readRDS("covid_ukb9512_full_40GPCs_updated_06_17_21.RDS")
 
-ukb26323 <- read_delim("/n/groups/patel/uk_biobank/pheno_26323/ukb26323.tab",delim="\t")
+ukb26323 <- read_delim("ukb26323.tab",delim="\t")
 
 ## create dataframe with age column and generate age^2 column
 age_data <- ukb26323 %>% select(f.eid,f.21022.0.0) %>% mutate(Age = f.21022.0.0, Age_squared = f.21022.0.0^2)
@@ -48,11 +54,11 @@ covid_hosp_status_data_processed <- bind_cols(eid = covid_test_patient_ids, covi
 
 covid_ukb9512_full_40GPCs_updated_age_data <- left_join(covid_ukb9512_full_40GPCs_updated_age_data,covid_hosp_status_data_processed,by="eid")
 
-saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_hosp_status_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"covid_hosp_status_ukb9512_full_40GPCs_updated_06_17_21.RDS")
 
-covid_ukb9512_full_40GPCs_updated_age_data <- readRDS("/n/scratch3/users/s/st320/UKB_COVID_XWAS/covid_hosp_status_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+covid_ukb9512_full_40GPCs_updated_age_data <- readRDS("covid_hosp_status_ukb9512_full_40GPCs_updated_06_17_21.RDS")
 
-death_cause_data <- read_delim("/home/st320/UKB_COVID_Analysis_02_21/death_cause_020721.txt",delim = "\t")
+death_cause_data <- read_delim("death_cause_020721.txt",delim = "\t")
 
 ## filter for death due to COVID 19 (ICD-10 codes: U07.1 AND U07.2)
 covid_death_cause_data  <- death_cause_data %>% filter(cause_icd10 %in% c("U071","U072"))
@@ -70,11 +76,11 @@ covid_death_data_processed <- as.data.frame(cbind(eid = all_death_data_patient_i
 
 covid_ukb9512_full_40GPCs_updated_age_data <- left_join(covid_ukb9512_full_40GPCs_updated_age_data,covid_death_data_processed,by="eid")
 
-saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+saveRDS(covid_ukb9512_full_40GPCs_updated_age_data,"covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
 
 ## update dataframe with correct variable codings
 
-data <- readRDS("/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+data <- readRDS("covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
 
 ethnicity_categ_to_remove <- names(table(data$f.21000.0.0)[which(table(data$f.21000.0.0) < 20)])
 data  <- data  %>% filter(! f.21000.0.0 %in% c("Do not know","Prefer not to answer","Other ethnic group",ethnicity_categ_to_remove))
@@ -93,4 +99,4 @@ data$x_738 <- relevel(data$x_738,ref = 5)
 
 data$f.31.0.0 <- factor(data$f.31.0.0,ordered=F)
 
-saveRDS(data,"/n/groups/patel/sivateja/UKB/COVID19/covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
+saveRDS(data,"covid_death_ukb9512_full_40GPCs_updated_06_17_21.RDS")
